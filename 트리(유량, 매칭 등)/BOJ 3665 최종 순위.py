a437967472753b0dd625fb2a3a05dfd956571ef6
LONGNEW <shinhyoung26@gmail.com>
2022-08-30 19:20:26 +0900

    2022.08.30

--- 트리(유량, 매칭 등)/BOJ 3665 최종 순위.py
+++ 트리(유량, 매칭 등)/BOJ 3665 최종 순위.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 t = int(sys.stdin.readline())
 for _ in range(t):
@@ -6,40 +7,49 @@ for _ in range(t):
     prev = list(map(int, sys.stdin.readline().split()))
     m = int(sys.stdin.readline())
     degree = [0] * (n + 1)
+    graph = [dict() for _ in range(n + 1)]
 
     for i in range(n - 1, 0, -1):
         node = prev[i]
         degree[node] = i
 
+        for j in range(i - 1, -1, -1):
+            next_node = prev[j]
+            graph[next_node][node] = 1
+
+
     now_degree = [i for i in degree]
     for i in range(m):
         a, b = map(int, sys.stdin.readline().split())
         if degree[a] > degree[b]:
+            del graph[b][a]
+            graph[a][b] = 1
             now_degree[a] -= 1
             now_degree[b] += 1
         else:
+            del graph[a][b]
+            graph[b][a] = 1
             now_degree[b] -= 1
             now_degree[a] += 1
 
-    temp = []
-    for node in range(1, n + 1):
-        deg = now_degree[node]
-        temp.append((deg, node))
+    q = deque([])
+    for i in range(1, n + 1):
+        if now_degree[i] == 0:
+            q.append(i)
 
-    temp.sort()
-    flag = 0
-    for i in range(n - 1):
-        if flag:
-            continue
+    ans = []
+    while q and len(q) < 2:
+        node = q.popleft()
+        ans.append(node)
 
-        deg1, deg2 = temp[i][0], temp[i + 1][0]
-        if deg1 == deg2:
-            flag = 1
+        for next_node in graph[node].keys():
+            now_degree[next_node] -= 1
+            if now_degree[next_node] == 0:
+                q.append(next_node)
 
-    if flag:
+    if len(q) > 1:
+        print("?")
+    elif len(ans) != n:
         print("IMPOSSIBLE")
-        continue
-
-    for deg, node in temp:
-        print(node, end=" ")
-    print()
+    else:
+        print(*ans)
